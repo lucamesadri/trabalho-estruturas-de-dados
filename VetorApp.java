6212114603fd65e11446ba047eaf6c94291cb5b9
Luca Vinícius Mesadri <101848988+lucamesadri@users.noreply.github.com>
2025-04-03 23:02:32 -0300

    Update VetorApp.java

--- VetorApp.java
+++ VetorApp.java
@@ -7,22 +7,6 @@ public class VetorApp {
     public static void main(String[] args) {
         Vetor vetor = new Vetor(10);
         Lista lista = new Lista();
-        
-        /* 
-        vetor.adicionar(2);
-        vetor.adicionar(7);
-        vetor.adicionar(9);
-        vetor.adicionar(12);
-        vetor.adicionar(16);
-        vetor.adicionar(21);
-        vetor.adicionar(27);
-        vetor.adicionar(33);
-        vetor.adicionar(42);
-        vetor.adicionar(54);
-        System.out.println("-----");
-        vetor.buscabinaria(54);
-        vetor.adicionaraoinicio(2);
-        System.out.println("-----");*/
         lista.adicionarAoInicio(2);
         lista.adicionarAoInicio(7);
         lista.adicionarAoInicio(9);
@@ -34,10 +18,10 @@ public class VetorApp {
         lista.adicionarAoInicio(42);
         lista.adicionaraofinal(54);
         System.out.println("-----");
-        // lista.adicionarnovalor(10, 12);
-        // lista.mostrarNos();
-        // lista.numeroNos();
-        // System.out.println(lista.tamanho);
+        lista.adicionarnovalor(10, 12);
+        lista.mostrarNos();
+        lista.numeroNos();
+        System.out.println(lista.tamanho);
         System.out.println("-----");  
         Iterador iterador = new Iterador(lista, lista.inicio);
         iterador.inserirAposAtual(89);
